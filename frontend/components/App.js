81e7c05f6c0fc764ca17652ce56d206104cdaa4a
Robert Medina <robertsmedina11@gmail.com>
2024-02-09 02:16:54 -0500

    final project finished

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
+
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
+const [starz, setStarz] = useState({people: [], planets: []});
+
+useEffect(() => {
+ function peopleplanets (){
+  axios.all([axios.get(urlPeople), axios.get(urlPlanets)])
+
+  .then(axios.spread((peopleResponse, planetsResponse) =>{
+    const peopleData = peopleResponse.data;
+    const planetsData = planetsResponse.data;
+    setStarz({people: peopleData, planets: planetsData});
+   
+    
+  }))
+
+  .catch(err =>{
+    console.log(err.message)
+  })
+ }
+  peopleplanets();
+})
+
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
   return (
     <div>
       <h2>Star Wars Characters</h2>
+      
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+            {starz.people.map(person => (
+              <Character key={person.name} data={person} planets = {starz.planets} />
+            ))}
+
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
-function Character() { // ❗ Add the props
+
+
+// how to properly fetch data from the App to use to match information ****
+
+function Character({data, planets}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+    const [showHomeworld, setshowHomeworld] = useState(false);
+    const [homeworldName, setHomeworldname]= useState('');
+
+    useEffect(() => {
+
+      const fetchHomeworldName =  () => {
+
+          const homeworld = planets.find(planet => planet.id === data.homeworld);
+          if(homeworld){
+            setHomeworldname(homeworld.name)
+          }
+        };
+
+        if(showHomeworld){
+          fetchHomeworldName();
+        }
+      }), [data.homeworld,planets,showHomeworld];
+
+    const toggleWorld = () => {
+
+      setshowHomeworld(!showHomeworld);
+    }
+
+   
+
+
+
   return (
-    <div>
+    <div className = "character-card" onClick={toggleWorld} >
       {/* Use the same markup with the same attributes as in the mock */}
+
+      <h3 className = "character-name" >{data.name}</h3>
+      {showHomeworld &&  <p className = "character-planet"> Planet: {homeworldName || 'loading...'}</p>}
+     
+      
     </div>
   )
 }
